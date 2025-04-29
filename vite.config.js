5b93da7d4d44a5dbaa035fc3360d6130ec888b79
phongnguyenthe <phong.nguyenthe@amela.vn>
2025-04-29 14:16:31 +0700

    Fix CiCD

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: "/react-app/",
+    base: "/portfolio/dist/",
 });
