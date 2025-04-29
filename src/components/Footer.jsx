3ddf347f9abb283c5f1d025ebc568dceae2e1ac5
phongnguyenthe <phong.nguyenthe@amela.vn>
2025-04-29 13:46:04 +0700

    Check CI/CI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -146,7 +146,7 @@ const Footer = () => {
 
                 <div className="border-t border-gray-800 mt-10 pt-6 text-left">
                     <p className="text-gray-400 text-sm">
-                        &copy; {currentYear} PhongNT. All rights reserved.
+                        &copy; {currentYear} Nguyen The Phong. All rights reserved.
                     </p>
                 </div>
             </div>
